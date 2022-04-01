b3aa82925d28e4f4126fcadd671c1b2b2166aa4d
Riki Miyakawa <riki.miyakawa@findex.co.jp>
2022-04-01 23:50:27 +0900

    pages用資源のバックアップ

---
+++ resources/updateIndex.rb
@@ -0,0 +1,34 @@
+# index.html更新プログラム
+require 'yaml'
+require 'fileutils'
+
+index_src = 'resources/index.org'   #コピー元index.html
+index_dest = 'index.html'           #コピー先index.html
+
+# テーブル列を作成
+def populate_html( fName, name, branch, version, description, published)
+    return '<tr><td>{name}</td><td>{version}</td><td>{branch}</td><td>{published}</td><td><a href="{fName}index.html">ページ</a></td><td><a href="{fName}qa.html">警告</a></td></tr>'
+    .gsub(/{fName}/, fName).gsub(/{name}/,name).gsub(/{version}/, version.to_s).gsub(/{branch}/, branch).gsub(/{published}/,published)
+end
+
+p 'メイン処理開始'
+content = ''
+p 'フォルダより_index.ymlを検索'
+for im in Dir.glob('./*/*/_index.yml')
+    File.open(im) do |f|
+        h = YAML.load(f)
+        content << (populate_html(im.sub('_index.yml',''), h[0]['name'], h[4]['branch'], h[1]['version'], h[2]['description'],h[3]['last_published'].to_s))
+    end
+end
+
+p 'コピー元取得'
+page = ''
+File.open(index_src) do |fr|
+    page = fr.read()
+    page = page.sub('{table content}', content)
+end
+
+p 'コピー先取得'
+File.open(index_dest, 'w') do |fw|
+    fw.puts page
+end
